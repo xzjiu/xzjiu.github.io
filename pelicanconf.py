9eb5e25ea0944e6421e0d8779f871e892923f6e6
zouxinrui <853836997@qq.com>
2022-11-07 00:33:13 -0500

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,50 @@
+AUTHOR = 'xinrui'
+SITENAME = "Xinrui's blog"
+SITEURL = ''
+
+THEME = 'themes/attila'
+OUTPUT_PATH = 'output'
+PATH = 'content'
+
+DEFAULT_CATEGORY = 'Assignment'
+
+TIMEZONE = 'America/New_York'
+
+AUTHORS_BIO = {
+  "xinrui": {
+    "name": "Xinrui Zou",
+    "cover": "https://images.unsplash.com/photo-1510146758428-e5e4b17b8b6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "image": "https://i.pinimg.com/564x/eb/d0/02/ebd002f69ccba8c11d7a35aef69a820e.jpg",
+    "website": "https://arulraj.net",
+    "linkedin": "arulrajnet",
+    "github": "arulrajnet",
+    "location": "Baltimore",
+    "bio": "Hahaha I am Xinrui! Bazinga!!"
+  }
+}
+
+DEFAULT_LANG = 'en'
+
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
